6facb79fbe87c9faf925016ec7ce1310208b6697
Vishnu Banna <banna3vishnu@gmail.com>
2021-07-07 09:25:26 -0700

    params update

--- yolo/dataloaders/yolo_input_test.py
+++ yolo/dataloaders/yolo_input_test.py
@@ -31,7 +31,7 @@ def test_yolo_input_task():
   experiment = "yolo_custom"
   # config_path = ["yolo/configs/experiments/yolov4/debug/jitter-scale/512-jitter-scale-ema.yaml"]
   # config_path = ["yolo/configs/experiments/yolov4/debug/jitter-scale/608-jitter-scale-ema.yaml"]
-  config_path = ["yolo/configs/experiments/yolov4-csp/debug/512-v4-aug.yaml"]
+  config_path = ["yolo/configs/experiments/yolov4-csp/debug/640-v4-aug.yaml"]
   # config_path = ["yolo/configs/experiments/yolov4/debug/state-test/512-state-test-ema.yaml"]
   # config_path = ["yolo/configs/experiments/yolov4-csp/inference/512-baseline.yaml"]
   # onfig_path = ["yolo/configs/experiments/yolov4/debug/512-jitter-scale-lthresh.yaml"]
