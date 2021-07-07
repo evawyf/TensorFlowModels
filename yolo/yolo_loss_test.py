578168cfe8638a2ad6aa9dd26e92520763eed017
Vishnu Banna <banna3vishnu@gmail.com>
2021-07-07 02:04:32 -0700

    new tests

--- yolo/yolo_loss_test.py
+++ yolo/yolo_loss_test.py
@@ -39,7 +39,8 @@ python3.8 -m yolo.run_image
 if __name__ == "__main__":
   task, model, params = load_model(
       experiment="yolo_custom",
-      config_path=["yolo/configs/experiments/yolov4-csp/inference/512-baseline.yaml"],
+      # config_path=["yolo/configs/experiments/yolov4-csp/inference/512-baseline.yaml"],
+      config_path=["yolo/configs/experiments/yolov4/inference/512-baseline.yaml"],
       model_dir="")
       # model_dir='/home/vbanna/Research/checkpoints/yolov4-csp/tpu/512')
       # model_dir='/home/vbanna/Research/checkpoints/250k-512-lr-special-t2')
@@ -57,12 +58,12 @@ if __name__ == "__main__":
   # if obj_mask.shape[1] == 16:
   #    plt.show()
 
-  lim = 1
+  lim = 3
   for p, image in enumerate(test_data):
     if p >= lim:
       break
     i, k = image 
     id = k['source_id'][0]
     # plt.imshow(tf.cast(i[0], tf.float32).numpy())
-    a = task.train_step(image, model, optimizer)
+    a = task.validation_step(image, model)
     print(id.numpy(), a['loss'].numpy())
